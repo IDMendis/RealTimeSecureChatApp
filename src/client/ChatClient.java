41eeb83ea5fc3207a46ad186e58adc3eeb2babe9
IDMendis <dinushimendisxp@gmail.com>
2025-10-31 16:23:49 +0530

    Project initialized with folder structure and starter classes for client and server.

---
+++ src/client/ChatClient.java
@@ -0,0 +1,7 @@
+package client;
+
+public class ChatClient {
+    public static void main(String[] args) {
+        System.out.println("Chat Client initialized...");
+    }
+}
---
+++ src/server/ChatServer.java
@@ -0,0 +1,12 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+
+
+public class ChatServer {
+    public static void main(String[] args) {
+        System.out.println("Chat Server initialized...");
+    }
+}
+javac
